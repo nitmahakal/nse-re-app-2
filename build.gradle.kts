d06c245e3396e4cb5ec993a0e858cc771fb7407a
nitmahakal <nitmahakal@gmail.com>
2026-09-16 17:02:45 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
-    id("com.android.application") version "9.0.0" apply false
-    id("org.jetbrains.kotlin.android") version "2.3.21" apply false
+    id("com.android.application") version "9.2.1" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.3.21" apply false
     id("com.chaquo.python") version "17.0.0" apply false
 }
