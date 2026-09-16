d9872c41cc9af7644ecd9a72f750f2c65a5cb030
nitmahakal <nitmahakal@gmail.com>
2026-09-16 19:27:51 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.nitmahakal.nsemarketresearch"
-    compileSdk = 36
+    compileSdk = 37
 
     defaultConfig {
         applicationId = "com.nitmahakal.nsemarketresearch"
